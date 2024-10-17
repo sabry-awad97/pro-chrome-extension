c92d49c5ad03765c1e965f76d159c3742d04d593
Sabry Awad <dr.sabry97@gmail.com>
2024-10-17 16:34:31 +0300

    🎉 feat(manifest.json): Add Chrome extension manifest file 🐛 fix(package.json): Update devDependency for '@crxjs/vite-plugin' 🗑️ remove(public/manifest.json): Delete unnecessary manifest file 🐛 fix(vite.config.ts): Import and use 'crx' plugin for manifest generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import manifest from "./manifest.json" with { type: "json" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
   server: {
     port: 3000,
   },
